a9487f950367eebe65257c687483ebc306af8607
Yujin Kim <yujin.kim@hotmail.fr>
2023-07-04 12:26:09 +0100

    start management

---
+++ trendyqc/trend_monitoring/management/commands/initial_import.py
@@ -0,0 +1,33 @@
+from django.core.management.base import BaseCommand
+from ._dnanexus_utils import search_multiqc_data, get_all_002_projects
+
+
+class Command(BaseCommand):
+    help = "Initial import of the MultiQC reports"
+
+    def add_arguments(self, parser):
+        parser.add_argument(
+            "-p_id", "--project_id",
+            help=(
+                "Project id(s) from which to import MultiQC reports. Mainly "
+                "for testing purposes"
+            )
+        )
+        parser.add_argument(
+            "-a", "--all", action="store_true",
+            help="Scan all 002 projects to import all MultiQC reports"
+        )
+
+    def handle(self, *args, **options):
+        project_ids = None
+
+        if options["p_id"]:
+            project_ids = options["p_id"]
+
+        if options["all"]:
+            project_ids = get_all_002_projects()
+
+        assert project_ids is not None, "Please use -a or -p_id"
+
+        for p_id in project_ids:
+            report_ids = search_multiqc_data(p_id)
