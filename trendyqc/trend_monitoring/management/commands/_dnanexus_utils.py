c4b25b23bef29413c4d43c7bd5ea495babb4eed5
Yujin Kim <yujin.kim@hotmail.fr>
2023-07-04 12:26:09 +0100

    parent 0d3c7935cf677dba45ef03a481b559404d803e39 author Yujin Kim <yujin.kim@hotmail.fr> 1688469969 +0100 committer Yujin Kim <yujin.kim@hotmail.fr> 1689085220 +0100
    parent 0d3c7935cf677dba45ef03a481b559404d803e39
    author Yujin Kim <yujin.kim@hotmail.fr> 1688469969 +0100
    committer Yujin Kim <yujin.kim@hotmail.fr> 1689085146 +0100
    
    parent 0d3c7935cf677dba45ef03a481b559404d803e39
    author Yujin Kim <yujin.kim@hotmail.fr> 1688469969 +0100
    committer Yujin Kim <yujin.kim@hotmail.fr> 1689085093 +0100
    
    parent 0d3c7935cf677dba45ef03a481b559404d803e39
    author Yujin Kim <yujin.kim@hotmail.fr> 1688469969 +0100
    committer Yujin Kim <yujin.kim@hotmail.fr> 1689084982 +0100
    
    parent 0d3c7935cf677dba45ef03a481b559404d803e39
    author Yujin Kim <yujin.kim@hotmail.fr> 1688469969 +0100
    committer Yujin Kim <yujin.kim@hotmail.fr> 1689084931 +0100
    
    parent 0d3c7935cf677dba45ef03a481b559404d803e39
    author Yujin Kim <yujin.kim@hotmail.fr> 1688469969 +0100
    committer Yujin Kim <yujin.kim@hotmail.fr> 1689084854 +0100
    
    parent 0d3c7935cf677dba45ef03a481b559404d803e39
    author Yujin Kim <yujin.kim@hotmail.fr> 1688469969 +0100
    committer Yujin Kim <yujin.kim@hotmail.fr> 1689084805 +0100
    
    parent 0d3c7935cf677dba45ef03a481b559404d803e39
    author Yujin Kim <yujin.kim@hotmail.fr> 1688469969 +0100
    committer Yujin Kim <yujin.kim@hotmail.fr> 1689084750 +0100
    
    start management
    
    fix option key
    
    nargs for project ids
    
    dxpy added
    
    login to dnanexus
    
    hide secret key, dx token loading
    
    login to dnanexus function
    
    match function name
    
    multiqc data searching function
    
    start management
    
    remove multiqc files
    
    rename search multiqc reports function
    
    get all 002 projects
    
    start parsing of multiqc report
    
    rename report id by report object
    
    read in multiqc report content
    
    init files
    
    nargs for project ids
    
    dxpy added
    
    hide secret key, dx token loading
    
    remove multiqc files
    
    init files
    
    nargs for project ids
    
    dxpy added
    
    hide secret key, dx token loading
    
    dotenv added to requirements
    
    init files
    
    get all 002 projects
    
    rename report id by report object
    
    read in multiqc report content
    
    mapping jsons for field matching

---
+++ trendyqc/trend_monitoring/management/commands/_dnanexus_utils.py
@@ -0,0 +1,41 @@
+import dxpy
+
+from django.conf import settings
+
+
+def login_to_dnanexus():
+    DX_SECURITY_CONTEXT = {
+        "auth_token_type": "Bearer",
+        "auth_token": settings.DX_TOKEN,
+    }
+
+    dxpy.set_security_context(DX_SECURITY_CONTEXT)
+
+    try:
+        dxpy.api.system_whoami()
+        print("DNAnexus login successful")
+    except Exception as err:
+        print(err)
+
+
+def search_multiqc_reports(project_id):
+    data = []
+
+    files = dxpy.find_data_objects(
+        project=project_id, name="multiqc_data.json"
+    )
+
+    for file in files:
+        data.append(dxpy.DXFile(file["id"], file["project"]))
+
+    return data
+
+
+def get_all_002_projects():
+    prod_projects = []
+    projects = dxpy.find_projects(name="^002", name_mode="regexp")
+
+    for project in projects:
+        prod_projects.append(project["id"])
+
+    return prod_projects
---
+++ trendyqc/trend_monitoring/management/commands/initial_import.py
@@ -0,0 +1,52 @@
+from django.core.management.base import BaseCommand
+
+import regex
+
+from ._dnanexus_utils import (
+    login_to_dnanexus, search_multiqc_reports, get_all_002_projects
+)
+from ._parsing import parse_multiqc_report
+
+
+class Command(BaseCommand):
+    help = "Initial import of the MultiQC reports"
+
+    def add_arguments(self, parser):
+        parser.add_argument(
+            "-p_id", "--project_id", nargs="+",
+            help=(
+                "Project id(s) from which to import MultiQC reports. Mainly "
+                "for testing purposes"
+            )
+        )
+        parser.add_argument(
+            "-a", "--all", action="store_true",
+            help="Scan all 002 projects to import all MultiQC reports"
+        )
+
+    def handle(self, *args, **options):
+        project_ids = None
+
+        login_to_dnanexus()
+
+        if options["project_id"]:
+            project_ids = options["project_id"]
+
+        if options["all"]:
+            project_ids = get_all_002_projects()
+
+        assert project_ids is not None, "Please use -a or -p_id"
+
+        data = {}
+
+        for p_id in project_ids:
+            assert regex.fullmatch(r"project-[a-zA-Z0-9]{24}", p_id), (
+                f"{p_id} is not a correctly formatted DNAnexus project id"
+            )
+            report_objects = search_multiqc_reports(p_id)
+
+            data[p_id] = report_objects
+
+        for p_id in data:
+            for report_object in data[p_id]:
+                report_data = parse_multiqc_report(report_object)
