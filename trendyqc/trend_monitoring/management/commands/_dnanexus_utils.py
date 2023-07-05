ab77dca32696cfe8855d51f4b9651e32d51fc9ae
Yujin Kim <yujin.kim@hotmail.fr>
2023-07-05 14:22:12 +0100

    login to dnanexus function

--- trendyqc/trend_monitoring/management/commands/_dnanexus_utils.py
+++ trendyqc/trend_monitoring/management/commands/_dnanexus_utils.py
@@ -1,3 +1,23 @@
+import dxpy
+
+from django.conf import settings
+
+
+def login_to_dnanexus():
+    DX_SECURITY_CONTEXT = {
+        "auth_token_type": "Bearer",
+        "auth_token": settings.DX_TOKEN,
+    }
+
+    dxpy.set_security_context(DX_SECURITY_CONTEXT)
+
+    try:
+        dxpy.api.system_whoami()
+        print("DNAnexus login successful")
+    except Exception as err:
+        print(err)
+
+
 def search_multiqc_data(project_id):
     pass
 
