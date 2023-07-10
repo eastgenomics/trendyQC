32156fae1130915c97c52fb1a33e81cefd9c4db8
Yujin Kim <yujin.kim@hotmail.fr>
2023-07-10 16:15:30 +0100

    start parsing of multiqc report

--- trendyqc/trend_monitoring/management/commands/initial_import.py
+++ trendyqc/trend_monitoring/management/commands/initial_import.py
@@ -5,6 +5,7 @@ import regex
 from ._dnanexus_utils import (
     login_to_dnanexus, search_multiqc_reports, get_all_002_projects
 )
+from ._parsing import parse_multiqc_report
 
 
 class Command(BaseCommand):
@@ -36,8 +37,16 @@ class Command(BaseCommand):
 
         assert project_ids is not None, "Please use -a or -p_id"
 
+        data = {}
+
         for p_id in project_ids:
             assert regex.fullmatch(r"project-[a-zA-Z0-9]{24}", p_id), (
                 f"{p_id} is not a correctly formatted DNAnexus project id"
             )
             report_ids = search_multiqc_reports(p_id)
+
+            data[p_id] = report_ids
+
+        for p_id in data:
+            for report_id in data[p_id]:
+                report_data = parse_multiqc_report(report_id)
