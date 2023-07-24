87d47cc073348c9aeec1e43b77bdf7d5439da645
Yujin Kim <yujin.kim@hotmail.fr>
2023-07-24 16:12:03 +0100

    remove tool config loading

--- trendyqc/trend_monitoring/management/commands/_parsing.py
+++ trendyqc/trend_monitoring/management/commands/_parsing.py
@@ -1,34 +1,8 @@
 import json
-from pathlib import Path
 from typing import Dict
 
-# returns the /trendyqc/trend_monitoring/management folder
-BASE_DIR_MANAGEMENT = Path(__file__).resolve().parent.parent
-CONFIG_DIR = BASE_DIR_MANAGEMENT / "configs"
 
-
-def load_tool_config(tool_name: str, subtool: str = None) -> Dict:
-    """ Read in the tool config and store it as a json
-
-    Args:
-        tool_name (str): Tool name i.e. Picard
-        subtool (str, optional): Subtool name i.e. hs_metrics
-        Defaults to None.
-
-    Returns:
-        dict: Dict containing the MultiQC fields and the fields from the models
-    """
-
-    tool_config = CONFIG_DIR / "tool_configs" / f"{tool_name}.json"
-    data = json.loads(tool_config.read_text())
-
-    if subtool:
-        return data[subtool]
-
-    return data
-
-
-def load_assay_config(assay_name: str) -> Dict:
+def load_assay_config(assay_name: str, config_dir) -> Dict:
     """ Read and load the assays.json
 
     Args:
@@ -40,7 +14,7 @@ def load_assay_config(assay_name: str) -> Dict:
         appropriate subsequent JSON
     """
 
-    assay_config = CONFIG_DIR / "assays.json"
+    assay_config = config_dir / "assays.json"
     data = json.loads(assay_config.read_text())
     assert assay_name in data, (
         f"{assay_name} is not present in the assay config file"
