60660fd57a8c159b826da37560d25cb54fc5eda8
Yujin Kim <yujin.kim@hotmail.fr>
2023-07-31 15:13:11 +0100

    starting backend for dashboard view

--- trendyqc/trend_monitoring/views.py
+++ trendyqc/trend_monitoring/views.py
@@ -1,9 +1,21 @@
 from django.views import View
 from django.shortcuts import render
+from trend_monitoring.models.metadata import Report, Report_Sample
 
 
 class Dashboard(View):
     template_name = "dashboard.html"
+    report_data = Report.objects.all()
+    report_sample_data = Report_Sample.objects.all()
 
     def get(self, request):
+        context = {"report_data": self.report_data}
+        return render(request, self.template_name, context)
+
+    def post(self, request, *args, **kwargs):
+        form = self.form_class(request.POST)
+
+        if form.is_valid():
+            pass
+
         return render(request, self.template_name)
