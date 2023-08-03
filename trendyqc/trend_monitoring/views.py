0558cc6a176cfe567e65b11cfabb1d1f5cdacd73
Yujin Kim <yujin.kim@hotmail.fr>
2023-08-03 12:22:43 +0100

    correct passing of data from Dashboard to Plot

--- trendyqc/trend_monitoring/views.py
+++ trendyqc/trend_monitoring/views.py
@@ -1,5 +1,6 @@
 from django.views import View
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import FilterForm
 from trend_monitoring.models.metadata import Report, Report_Sample
 
 
@@ -8,18 +9,32 @@ class Dashboard(View):
     report_data = Report.objects.all()
     report_sample_data = Report_Sample.objects.all()
 
-    def get(self, request):
+    def get_context_data(self):
         assays = sorted({
             "".join(assay)
             for assay in self.report_sample_data.values_list("assay")
         })
         context = {"report_data": self.report_data, "assays": assays}
+        return context
+
+    def get(self, request):
+        context = self.get_context_data()
         return render(request, self.template_name, context)
 
     def post(self, request, *args, **kwargs):
-        form = self.form_class(request.POST)
+        context = self.get_context_data()
+        form = FilterForm(request.POST)
 
         if form.is_valid():
-            pass
+            request.session["temp_data"] = form.cleaned_data
+            return redirect("Plot")
 
+        return render(request, self.template_name, context)
+
+
+class Plot(View):
+    template_name = "plot.html"
+
+    def get(self, request, *args, **kwargs):
+        form_data = request.session.get("temp_data")
         return render(request, self.template_name)
