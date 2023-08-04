7f0d787a76b662edc041c170f4b6bc1fd757dfd9
Yujin Kim <yujin.kim@hotmail.fr>
2023-08-04 10:33:57 +0100

    comments + renaming

--- trendyqc/trend_monitoring/views.py
+++ trendyqc/trend_monitoring/views.py
@@ -10,6 +10,13 @@ class Dashboard(View):
     report_sample_data = Report_Sample.objects.all()
 
     def get_context_data(self):
+        """ Get the basic data that needs to be displayed in the dashboard page
+
+        Returns:
+            dict: Dict of report and assay data to be passed to the dashboard
+        """
+
+        # get all the assays and sort them
         assays = sorted({
             "".join(assay)
             for assay in self.report_sample_data.values_list("assay")
@@ -18,14 +25,34 @@ class Dashboard(View):
         return context
 
     def get(self, request):
+        """ Handle GET request
+
+        Args:
+            request (?): HTML request coming in
+
+        Returns:
+            ?: Render Django thingy?
+        """
+
         context = self.get_context_data()
         return render(request, self.template_name, context)
 
     def post(self, request, *args, **kwargs):
+        """ Handle POST request
+
+        Args:
+            request (?): HTML request coming in
+
+        Returns:
+            ?: Render Django thingy? | Redirect thingy towards the Plot view
+        """
+
         context = self.get_context_data()
         form = FilterForm(request.POST)
 
+        # call the clean function and see if the form data is valid
         if form.is_valid():
+            # save the cleaned data in the session so that 
             request.session["form"] = form.cleaned_data
             return redirect("Plot")
 
@@ -36,15 +63,35 @@ class Plot(View):
     template_name = "plot.html"
 
     def get(self, request):
+        """ Handle GET request. This should only happen after the form has been
+        submitted.
+
+        Args:
+            request (?): HTML request
+
+        Returns:
+            ?: Render Django thingy
+        """
+
+        # check if we have the form data in the session request
         if request.session.get("form"):
-            filter_recap = self.filter_recap(request)
+            # clean the form data
+            filter_recap = self.clean_form_data(request.session.get("form"))
             context = {"data": filter_recap}
             return render(request, self.template_name, context)
 
         return render(request, self.template_name)
 
-    def filter_recap(self, request):
-        form_data = request.session.get("form")
+    def clean_form_data(self, form_data):
+        """ Clean form data by removing empty values
+
+        Args:
+            form_data (dict): Dict containing data from the Dashboard form
+
+        Returns:
+            dict: Dict of cleaned form data
+        """
+
         cleaned_plot_data = {
             key: value
             for key, value in form_data.items() if value
