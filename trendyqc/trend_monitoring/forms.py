780b6d49eb70e08096f474ed505583f722c962fb
Yujin Kim <yujin.kim@hotmail.fr>
2023-08-07 15:49:59 +0100

    clean dates for subset selection

--- trendyqc/trend_monitoring/forms.py
+++ trendyqc/trend_monitoring/forms.py
@@ -19,7 +19,9 @@ class FilterForm(forms.Form):
         run_subset = [
             cleaned_data.get("assay_select", None),
             cleaned_data.get("run_select", None),
-            cleaned_data.get("sequencer_select", None)
+            cleaned_data.get("sequencer_select", None),
+            cleaned_data.get("date_start", None),
+            cleaned_data.get("date_end", None)
         ]
 
         start_date = cleaned_data.get("date_start", None)
@@ -28,29 +30,21 @@ class FilterForm(forms.Form):
         if not any(run_subset):
             self.add_error(None, ValidationError("No subset of runs selected"))
 
-        x_metric = [
-            cleaned_data.get("metrics_x", None),
-            cleaned_data.get("date_start", None)
-        ]
+        if not end_date and start_date:
+            now = datetime.date.today()
+            cleaned_data["date_end"] = now
+            end_date = now
 
-        if not x_metric:
-            self.add_error(None, ValidationError("No X-axis values selected"))
-        else:
-            if not end_date and start_date:
-                now = datetime.date.today()
-                cleaned_data["date_end"] = now
-                end_date = now
-
-            if end_date and start_date:
-                if end_date < start_date:
-                    self.add_error(
-                        None, ValidationError(
-                            (
-                                "Date end cannot be smaller than date start: "
-                                f"{start_date} - {end_date}"
-                            )
+        if end_date and start_date:
+            if end_date < start_date:
+                self.add_error(
+                    None, ValidationError(
+                        (
+                            "Date end cannot be smaller than date start: "
+                            f"{start_date} - {end_date}"
                         )
                     )
+                )
 
         if not cleaned_data.get("metrics_y", None):
             self.add_error(None, ValidationError("No Y-axis metric selected"))
