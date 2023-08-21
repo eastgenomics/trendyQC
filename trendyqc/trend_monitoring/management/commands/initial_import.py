3c095237ba410ab456c23e1acba7d655ab9a9396
Yujin Kim <yujin.kim@hotmail.fr>
2023-08-21 15:22:39 +0100

    basic logging for initial import operations

--- trendyqc/trend_monitoring/management/commands/initial_import.py
+++ trendyqc/trend_monitoring/management/commands/initial_import.py
@@ -1,12 +1,17 @@
-from django.core.management.base import BaseCommand
+import logging
 
 import regex
+
+from django.core.management.base import BaseCommand
 
 from ._dnanexus_utils import (
     login_to_dnanexus, search_multiqc_reports, get_all_002_projects,
     is_archived
 )
 from ._multiqc import MultiQC_report
+
+logger = logging.getLogger(__name__)
+storing_logger = logging.getLogger("storing")
 
 
 class Command(BaseCommand):
@@ -44,18 +49,24 @@ class Command(BaseCommand):
         if options["all"]:
             project_ids = get_all_002_projects()
 
-        assert project_ids, "Please use -a or -p_id"
+        if not project_ids:
+            msg = "Please use -a or -p_id"
+            logger.error(msg)
+            raise Exception(msg)
+
+        archived_reports = []
 
         for p_id in project_ids:
-            assert regex.fullmatch(r"project-[a-zA-Z0-9]{24}", p_id), (
-                f"{p_id} is not a correctly formatted DNAnexus project id"
-            )
+            if not regex.fullmatch(r"project-[a-zA-Z0-9]{24}", p_id):
+                msg = f"{p_id} is not a correctly formatted DNAnexus project id"
+                logger.error(msg)
+                raise Exception(msg)
+
             report_objects = search_multiqc_reports(p_id)
 
             for report_object in report_objects:
                 if is_archived(report_object):
-                    # TO-DO: logging system. Generate list of projects skipped
-                    # to allow user to unarchive those to import in database ?
+                    archived_reports.append(report_object.multiqc_json_id)
                     continue
 
                 multiqc_report = MultiQC_report(report_object)
@@ -63,3 +74,16 @@ class Command(BaseCommand):
                 if multiqc_report.is_importable:
                     if not options["dry_run"]:
                         multiqc_report.import_instances()
+                        logger.info((
+                            f"Successfully imported: "
+                            f"{multiqc_report.multiqc_json_id}"
+                        ))
+                else:
+                    logger.warning((
+                        f"{multiqc_report.multiqc_json_id} doesn't have an "
+                        "assay name  present in the trendyqc/trend_monitoring/"
+                        "management/configs/assays.json"
+                    ))
+
+        if archived_reports:
+            storing_logger.warning(f"Archived reports: {archived_reports}")
