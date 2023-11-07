17753a9f529ae298168f600673d49c9c52ec7cf9
Yujin Kim <yujin.kim@hotmail.fr>
2023-11-07 14:49:25 +0000

    model for filters

---
+++ trendyqc/trend_monitoring/models/filters.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+
+class Filter(models.Model):
+    name = models.CharField(max_length=100)
+    content = models.TextField()
+
+    class Meta:
+        app_label = "trend_monitoring"
+        db_table = "filter"
