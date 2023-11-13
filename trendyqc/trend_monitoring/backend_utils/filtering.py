9a30bd99bad4b12eab97389be46d22f4325bf4e5
Yujin Kim <yujin.kim@hotmail.fr>
2023-11-13 14:43:17 +0000

    f-string instead of substitution

--- trendyqc/trend_monitoring/backend_utils/filtering.py
+++ trendyqc/trend_monitoring/backend_utils/filtering.py
@@ -56,4 +56,4 @@ def serialize_date(obj):
     if isinstance(obj, (datetime, date)):
         return obj.isoformat()
 
-    raise TypeError ("Type %s not serializable" % type(obj))
+    raise TypeError (f"Type {type(obj)} not serializable")
