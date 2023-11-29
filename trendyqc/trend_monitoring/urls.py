ec4869fb65e5692eb3f713d9967ed58469905846
Yujin Kim <yujin.kim@hotmail.fr>
2023-11-29 15:48:20 +0000

    redirect trendyqc/ to the dashboard

--- trendyqc/trend_monitoring/urls.py
+++ trendyqc/trend_monitoring/urls.py
@@ -5,7 +5,7 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path("dashboard/", views.Dashboard.as_view(), name="Dashboard"),
+    path("", views.Dashboard.as_view(), name="Dashboard"),
     path("plot/", views.Plot.as_view(), name="Plot"),
     path("logs/", include("log_viewer.urls")),
     path("accounts/", admin.site.urls),
