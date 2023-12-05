fcf2d6023206f8ced1156e63b5df83d746339deb
Yujin Kim <yujin.kim@hotmail.fr>
2023-12-05 16:32:59 +0000

    notify slack when big errors

--- trendyqc/trend_monitoring/management/commands/utils/_import.py
+++ trendyqc/trend_monitoring/management/commands/utils/_import.py
@@ -1,10 +1,12 @@
 import logging
+import traceback
 
 from django.apps import apps
 
 from ._check import already_in_db
 from ._dnanexus_utils import search_multiqc_reports, is_archived
 from ._multiqc import MultiQC_report
+from ._notifications import slack_notify
 
 logger = logging.getLogger("basic")
 storing_logger = logging.getLogger("storing")
@@ -43,12 +45,34 @@ def import_multiqc_reports(project_ids: list, dry_run: bool = False):
                 archived_reports.append(report_object.id)
                 continue
 
-            # this will fully setup the multiqc report to be ready for import
-            multiqc_report = MultiQC_report(report_object)
+            try:
+                # this will fully setup the multiqc report to be ready for import
+                multiqc_report = MultiQC_report(report_object)
+            except Exception:
+                msg = (
+                    f"TrendyQC - Failed to setup {report_object.id}\n"
+
+                    "```"
+                    f"{traceback.format_exc()}"
+                    "```"
+                )
+                slack_notify(msg)
 
             if multiqc_report.is_importable:
                 if not dry_run:
-                    multiqc_report.import_instances()
+                    try:
+                        multiqc_report.import_instances()
+                    except Exception:
+                        msg = (
+                            "TrendyQC - Failed to import "
+                            f"{multiqc_report.multiqc_json_id}\n"
+
+                            "```"
+                            f"{traceback.format_exc()}"
+                            "```"
+                        )
+                        slack_notify(msg)
+
                     logger.info((
                         f"Successfully imported: "
                         f"{multiqc_report.multiqc_json_id}"
