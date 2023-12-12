cee5967b6228755d687ca53ddc3fb9b529c05e40
Yujin Kim <yujin.kim@hotmail.fr>
2023-12-12 09:43:06 +0000

    add user to filter model

--- trendyqc/trend_monitoring/models/filters.py
+++ trendyqc/trend_monitoring/models/filters.py
@@ -3,6 +3,7 @@ from django.db import models
 
 class Filter(models.Model):
     name = models.CharField(max_length=100)
+    user = models.CharField(max_length=100)
     content = models.TextField()
 
     class Meta:
