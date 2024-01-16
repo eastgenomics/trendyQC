194ae400d301c7685d83cb05549de2c4627503bc
Yujin Kim <yujin.kim@hotmail.fr>
2024-01-16 12:06:39 +0000

    report id use + parameter change for multiqc obj

--- trendyqc/trend_monitoring/management/commands/utils/_import.py
+++ trendyqc/trend_monitoring/management/commands/utils/_import.py
@@ -33,8 +33,9 @@ def import_multiqc_reports(project_ids: list, dry_run: bool = False):
         report_objects = search_multiqc_reports(p_id)
 
         for report_object in report_objects:
+            report_id = report_object.id
             report_kwargs = {
-                "dnanexus_file_id": report_object.id, "project_id": p_id
+                "dnanexus_file_id": report_id, "project_id": p_id
             }
 
             # check if the report is already in the database
@@ -43,15 +44,20 @@ def import_multiqc_reports(project_ids: list, dry_run: bool = False):
 
             # check if the report is archived
             if is_archived(report_object):
-                archived_reports.append(report_object.id)
+                archived_reports.append(report_id)
                 continue
+
+            job_id = report_object.describe()["createdBy"]["job"]
+            report_data = report_object.read()
 
             try:
                 # this will fully setup the multiqc report to be ready for import
-                multiqc_report = MultiQC_report(report_object)
+                multiqc_report = MultiQC_report(
+                    report_id, p_id, job_id, report_data
+                )
             except Exception:
                 msg = (
-                    f"TrendyQC - Failed to setup {report_object.id}\n"
+                    f"TrendyQC - Failed to setup {report_id}\n"
 
                     "```"
                     f"{traceback.format_exc()}"
