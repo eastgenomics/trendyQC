f47a860f61a3d3d6d5946a65fd5b01b40c28df98
Yujin Kim <yujin.kim@hotmail.fr>
2024-07-11 12:57:30 +0100

    create directory if not exist

--- trendyqc_grafana.sh
+++ trendyqc_grafana.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+if [[ ! -d "/var/log/monitoring" ]]; then
+    mkdir -p /var/log/monitoring
+fi
+
 if [[ -f "/var/log/monitoring/trendyqc.prom" ]]; then
     rm /var/log/monitoring/trendyqc.prom
 fi
