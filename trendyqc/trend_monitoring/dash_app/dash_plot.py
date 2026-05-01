30fc701b0efa34d9e906e30c95b132478ad099b0
Yujin Kim <yujin.kim@hotmail.fr>
2026-05-01 16:39:57 +0100

    reorganise code + callback filtering for date info

--- trendyqc/trend_monitoring/dash_app/dash_plot.py
+++ trendyqc/trend_monitoring/dash_app/dash_plot.py
@@ -9,58 +9,44 @@ import dash
 import dash_bootstrap_components as dbc
 import dash_mantine_components as dmc
 import plotly.graph_objects as go
-from dash import dcc, html
+from dash import dcc, html, Output, Input
 from django_plotly_dash import DjangoDash
+
 from trend_monitoring.dash_app.get_data.filtering import (
     get_data_for_plotting,
     get_subset_queryset,
     format_data_for_plotly_js,
 )
-
-from trend_monitoring.dash_app.setup_dash_elements.dropdowns import (
-    get_metric_over_time_dropdowns,
-)
 from trend_monitoring.dash_app.setup_dash_elements.individual_dropdowns import (
     get_assay,
     get_metrics,
-)
-from trend_monitoring.dash_app.setup_dash_elements.tabs import (
-    get_metric_over_time_tab_content,
-    get_tabs,
+    get_date_picker,
 )
 
 app = DjangoDash("Plot", external_stylesheets=[dbc.themes.BOOTSTRAP])
 
-app.layout = dmc.MantineProvider(
-    html.Div(
-        [
-            dmc.Flex(
-                get_assay() + get_metrics(),
-                gap="md",
-                justify="center",
-                align="center",
-                direction="row",
-                wrap="wrap",
-            ),
-            dcc.Graph(id="output-graph", style={"height": "75vh"}),
-        ],
-        style={"padding": "10px"},
-    )
-)
-
 
 @app.callback(
-    dash.dependencies.Output("output-graph", "figure"),
+    Output("output-graph", "figure"),
     [
-        dash.dependencies.Input("dropdown-assay", "value"),
-        dash.dependencies.Input("dropdown-metric", "value"),
+        Input("dropdown-assay", "value"),
+        Input("dropdown-metric", "value"),
+        Input("radio-date", "value"),
+        Input("date-picker", "value"),
     ],
 )
-def callback_graph(assays, metrics):
-    if not assays or not metrics:
+def callback_graph(assays, metrics, days_back, date_range):
+    if metrics is None:
         return dash.no_update
 
-    data = get_subset_queryset({"assay": assays})
+    data = get_subset_queryset(
+        {
+            "assay": assays,
+            "date_start": date_range[0] if date_range else None,
+            "date_end": date_range[1] if date_range else None,
+            "days_back": int(days_back) if days_back else None,
+        }
+    )
     df, projects_no_metrics, samples_no_metric = get_data_for_plotting(
         data, metrics
     )
@@ -72,3 +58,34 @@ def callback_graph(assays, metrics):
         fig.add_trace(go.Box(**json_data))
 
     return fig
+
+
+def define_layout(**kwargs):
+    request = kwargs.get("request")
+    is_authenticated = request and request.user.is_authenticated
+
+    return dmc.MantineProvider(
+        html.Div(
+            [
+                dcc.Store(
+                    id={"type": "storage", "index": "session"},
+                    storage_type="session",
+                ),
+                dmc.Grid(
+                    [
+                        dmc.GridCol(get_assay(), span="auto"),
+                        dmc.GridCol(get_metrics(), span="auto"),
+                        dmc.GridCol(get_date_picker(), span="auto"),
+                    ],
+                    justify="space-between",
+                    align="center",
+                    grow=True,
+                ),
+                dcc.Graph(id="output-graph", style={"height": "75vh"}),
+            ],
+            style={"padding": "10px"},
+        )
+    )
+
+
+app.layout = define_layout
