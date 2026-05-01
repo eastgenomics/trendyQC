351e841328bcccfcab2125ebbd89011661b49d79
Yujin Kim <yujin.kim@hotmail.fr>
2026-05-01 08:56:46 +0100

    reorder imports

--- trendyqc/trendyqc/urls.py
+++ trendyqc/trendyqc/urls.py
@@ -1,7 +1,7 @@
 # trend_app URL Configuration
 
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import include, path
 
 urlpatterns = [
     path("trendyqc/admin/", admin.site.urls),
