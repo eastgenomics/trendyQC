8e1bfcaf2b71864bc0e284f5fc51807dd0ddbe33
Yujin Kim <yujin.kim@hotmail.fr>
2026-05-27 11:58:14 +0100

    auth status url for checking auth status in plot page

--- trendyqc/trend_monitoring/urls.py
+++ trendyqc/trend_monitoring/urls.py
@@ -11,4 +11,5 @@ urlpatterns = [
     path("login/", views.Login.as_view(), name="Login"),
     path("logout/", views.Logout.as_view(), name="Logout"),
     path("django_plotly_dash/", include("django_plotly_dash.urls")),
+    path("auth-status/", views.auth_status, name="auth_status"),
 ]
