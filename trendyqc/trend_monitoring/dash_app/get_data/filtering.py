628c11d4452ad98010a727d507f8b84bdb178aba
Yujin Kim <yujin.kim@hotmail.fr>
2026-05-29 12:42:47 +0100

    make outline darker

--- trendyqc/trend_monitoring/dash_app/get_data/filtering.py
+++ trendyqc/trend_monitoring/dash_app/get_data/filtering.py
@@ -594,7 +594,7 @@ def build_groups(df):
     )
 
 
-def darken_hex(hex_color: str, factor: float = 0.8) -> str:
+def darken_hex(hex_color: str, factor: float = 0.6) -> str:
     hex_color = hex_color.lstrip("#")
     r, g, b = (int(hex_color[i : i + 2], 16) for i in (0, 2, 4))
 
