347e2bbb8669101951e1cc905529201ec1a54fb5
Yujin Kim <yujin.kim@hotmail.fr>
2026-06-01 14:22:32 +0100

    remove dropdown grouping script

--- trendyqc/trend_monitoring/dash_app/setup_dash_elements/dropdowns.py
+++
@@ -1,7 +0,0 @@
-import dash_bootstrap_components as dbc
-from trend_monitoring.dash_app.setup_dash_elements.individual_dropdowns import (
-    get_assay, get_metrics)
-
-
-def get_metric_over_time_dropdowns():
-    return [get_assay(), get_metrics()]
