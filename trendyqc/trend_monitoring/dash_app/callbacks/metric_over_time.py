5739ad861bfc465434d6d3742c582269d482a204
Yujin Kim <yujin.kim@hotmail.fr>
2026-06-02 14:25:24 +0100

    move callback for metric over time to separate script

---
+++ trendyqc/trend_monitoring/dash_app/callbacks/metric_over_time.py
@@ -0,0 +1,238 @@
+import json
+import logging
+
+import dash
+import plotly.graph_objects as go
+from dash import Output, Input, State, ALL
+
+from trend_monitoring.dash_app.get_data.filtering import (
+    get_data_for_plotting,
+    get_subset_queryset,
+    format_data_for_boxplot,
+)
+from trend_monitoring.dash_app.get_data.annotations import add_annotations
+from trend_monitoring.dash_app.setup_dash_elements.individual_dropdowns import (
+    get_filter_table,
+)
+from trend_monitoring.dash_app.setup_dash_elements.utils import (
+    build_filter_text,
+)
+from trend_monitoring.dash_app.handle_filters import import_filter
+
+from trend_monitoring.models.filters import Filter
+
+logger = logging.getLogger("basic")
+
+
+def register_callback(app):
+    @app.callback(
+        Output("save-filter-btn", "style"),
+        Output("filter-table", "style"),
+        Input("auth-store", "data"),
+    )
+    def toggle_filter_components(is_authenticated):
+        if is_authenticated:
+            return {"display": "flex"}, {"display": "flex"}
+
+        return {"display": "none"}, {"display": "none"}
+
+    @app.callback(
+        Output("filter-table-container", "children"),
+        Input("auth-interval", "n_intervals"),
+        Input("filter-store", "data"),
+        Input("filter-saved-store", "data"),  # triggers refresh after delete
+    )
+    def refresh_filter_table(*args, **kwargs):
+        return get_filter_table()
+
+    @app.callback(
+        Output("filter-name-modal", "opened"),
+        Output("save-message-store", "data"),
+        Output("filter-saved-store", "data"),
+        Input("save-filter-btn", "n_clicks"),
+        Input("submit-filter_name", "n_clicks"),
+        State("filter-name", "value"),
+        State("filter-name-modal", "opened"),
+        State("dropdown-assay", "value"),
+        State("dropdown-metric", "value"),
+        State("radio-date", "value"),
+        State("date-picker", "value"),
+        State("filter-saved-store", "data"),
+        prevent_initial_call=True,
+    )
+    def save_filter(
+        save_button,
+        submit_name,
+        filter_name,
+        opened,
+        assays,
+        metrics,
+        days_back,
+        date_range,
+        filter_saved,
+        *args,
+        **kwargs,
+    ):
+        triggered = (
+            kwargs.get("callback_context")
+            .triggered[0]["prop_id"]
+            .split(".")[0]
+        )
+
+        msg_data = {}
+
+        if triggered == "save-filter-btn":
+            return True, msg_data, filter_saved
+
+        if triggered == "submit-filter_name":
+            form_data = {
+                "assay": assays,
+                "metric": metrics,
+                "days_back": [days_back],
+                "date_start": date_range[0] if date_range else None,
+                "date_end": date_range[1] if date_range else None,
+            }
+
+            if kwargs.get("request"):
+                request = kwargs["request"]
+                msg, msg_status = import_filter(
+                    filter_name, request.user.username, form_data
+                )
+                msg_data = {
+                    "attributes": {
+                        "label": "Filter saved",
+                        "message": msg,
+                    },
+                    "color": "green" if msg_status else "red",
+                    "hide": False,
+                }
+                logger.info(msg)
+
+            return False, msg_data, filter_saved + 1
+
+        return opened, msg_data, filter_saved
+
+    @app.callback(
+        Output("filter-store", "data"),
+        Output("delete-message-store", "data"),
+        Input({"type": "delete-filter-btn", "index": ALL}, "n_clicks"),
+        State("filter-store", "data"),
+        prevent_initial_call=True,
+    )
+    def delete_filter(n_clicks, current, *args, **kwargs):
+        if not any(n_clicks):
+            raise dash.exceptions.PreventUpdate
+
+        triggered = kwargs.get("callback_context").triggered[0]["prop_id"]
+        filter_id = json.loads(triggered.split(".")[0])["index"]
+
+        filter_to_delete = Filter.objects.get(id=filter_id)
+        filter_name = filter_to_delete.name
+        delete_msg = filter_to_delete.delete()
+
+        msg = f"Filter '{filter_name}' has been successfully deleted"
+        msg_data = {
+            "attributes": {
+                "label": "Filter deleted",
+                "message": msg,
+            },
+            "color": "green",
+            "hide": False,
+        }
+        logger.info(f"{msg}: {delete_msg}")
+
+        return (current + 1, msg_data)  # increment to trigger refresh
+
+    @app.callback(
+        Output("applied-filter-store", "data"),
+        Input("dropdown-assay", "value"),
+        Input("dropdown-metric", "value"),
+        Input("radio-date", "value"),
+        Input("date-picker", "value"),
+        Input({"type": "use-filter-btn", "index": ALL}, "n_clicks"),
+        prevent_initial_call=True,
+    )
+    def update_filter_store(
+        assays, metrics, days_back, date_range, n_clicks, *args, **kwargs
+    ):
+        triggered = kwargs.get("callback_context").triggered[0]["prop_id"]
+
+        if "use-filter-btn" in triggered:
+            if not any(n_clicks):
+                raise dash.exceptions.PreventUpdate
+            filter_id = json.loads(triggered.split(".")[0])["index"]
+            filter_obj = Filter.objects.get(id=filter_id)
+            return json.loads(filter_obj.content)
+
+        # store dropdown values directly
+        return {
+            "assay": assays,
+            "metric": metrics,
+            "days_back": [days_back],
+            "date_start": date_range[0] if date_range else None,
+            "date_end": date_range[1] if date_range else None,
+        }
+
+    @app.callback(
+        Output("metric-over-time-graph", "figure"),
+        Output("metric-over-time-graph-title", "children"),
+        Input("applied-filter-store", "data"),
+        Input("annotation-checkbox", "checked"),
+        prevent_initial_call=True,
+    )
+    def callback_graph(applied_filter, show_annotations):
+        if not applied_filter:
+            return dash.no_update, dash.no_update
+
+        assays = applied_filter.get("assay")
+        metrics = applied_filter.get("metric")
+        days_back = applied_filter.get("days_back", [None])[0]
+        date_range = [
+            applied_filter.get("date_start"),
+            applied_filter.get("date_end"),
+        ]
+
+        if not metrics:
+            return dash.no_update, dash.no_update
+
+        data = get_subset_queryset(
+            {
+                "assay": assays,
+                "date_start": date_range[0] if date_range else None,
+                "date_end": date_range[1] if date_range else None,
+                "days_back": int(days_back) if days_back else None,
+            }
+        )
+        df, projects_no_metrics, samples_no_metric = get_data_for_plotting(
+            data, metrics
+        )
+
+        json_plot_data, is_grouped = format_data_for_boxplot(df)
+
+        fig = go.Figure()
+        for json_data in json_plot_data:
+            fig.add_trace(go.Box(**json_data))
+
+        if show_annotations and df:
+            # get date range from the data
+            all_dates = [
+                row["date"]
+                for df_metric in df
+                for _, row in df_metric.iterrows()
+            ]
+            if all_dates:
+                fig = add_annotations(fig, min(all_dates), max(all_dates))
+
+        plot_title = build_filter_text(
+            json.dumps(
+                {
+                    "assay": assays,
+                    "metric": metrics,
+                    "date_start": date_range[0] if date_range else None,
+                    "date_end": date_range[1] if date_range else None,
+                    "days_back": [int(days_back)] if days_back else None,
+                }
+            )
+        )
+
+        return fig, plot_title
